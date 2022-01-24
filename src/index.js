e295161d8451b46b0c1d90288c25ad7b427ae18e
nao <nao_h13@hotmail.com>
2022-01-25 01:31:54 +0900

    履歴移動の実装

--- src/index.js
+++ src/index.js
@@ -68,12 +68,13 @@ class Board extends React.Component {
         history: [{
           squares: Array(9).fill(null)
         }],
+        stepNumber: 0,
         xIsNext: true
       };
     }
   
     handleClick(i) {
-      const history = this.state.history;
+      const history = this.state.history.splice( 0, this.state.stepNumber + 1 );
       const current = history[history.length - 1];
       const squares = current.squares.slice();
       if (calculateWinner(squares) || squares[i]) {
@@ -84,15 +85,32 @@ class Board extends React.Component {
         history: history.concat([{
           squares: squares
         }]),
+        stepNumber: history.length,
         xIsNext: !this.state.xIsNext,
       });
     }
-    
+    jumpTo( step ) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0,
+        });
+    }
     render() {
       const history = this.state.history;
-      const current = history[history.length - 1];
+      const current = history[ this.state.stepNumber ];
       const winner = calculateWinner(current.squares);
   
+      const moves = history.map(( step, move ) => {
+        const desc = move ?
+            'Go to Move #' + move :
+            'Go to game start';
+        return (
+            <li key={move}>
+                <button onClick={() => this.jumpTo( move )}>{desc}</button>
+            </li>
+        );
+      });
+
       let status;
       if (winner) {
         status = 'Winner: ' + winner;
@@ -111,7 +129,7 @@ class Board extends React.Component {
           </div>
           <div className="game-info">
             <div>{status}</div>
-            <ol>{/* TODO */}</ol>
+            <ol>{moves}</ol>
           </div>
         </div>
       );
@@ -148,6 +166,6 @@ class Board extends React.Component {
             return squares[ a ];
         }
       }
-      
+
       return null;
   }
